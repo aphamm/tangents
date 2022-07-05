02603e984a217fbdbc4fddf80182a842eb8f0ad8
pham <austinpham77@g.ucla.edu>
2022-07-05 00:37:52 -0700

    revised sorting ✍️

--- algos/sorting.py
+++ algos/sorting.py
@@ -1,135 +1,168 @@
-#################################################
-# insertion_sort
-#################################################
-# input: arr
-# idea: move first item to correct place, move second item, move third...
-# time: O(N^2) but O(N) if almost sorted
-# stable: yes
-# in-place: yes
+"""
+algo: insertion_sort
+input: arr
+idea: slide first item right until correct place, slide second, slide third...
+time: O(N^2) but O(N) if almost sorted
+stable: yes
+in-place: yes
+"""
 
 def insertion_sort(arr):
     for i in range(len(arr)):
+        # only look at current i
         cur = i
+        # slide it right until in correct place
         while cur > 0 and arr[cur] < arr[cur-1]:
             arr[cur], arr[cur-1] = arr[cur-1], arr[cur]
             cur -= 1
     return arr
 
-def test_insertion_sort():
-  print(insertion_sort([1, 9, 2, 6, -1, 4, 4, 8, 2]))
+def test1():
+  assert insertion_sort([1, 9, 2, 6, -1, 4]) == [-1, 1, 2, 4, 6, 9]
+  assert insertion_sort([4, 1, 5, 7, 1, 2]) == [1, 1, 2, 4, 5, 7]
+  assert insertion_sort([9, 8, 7, 6]) == [6, 7, 8, 9]
+  print("Test Cases Passed!")
 
-#################################################
-# selection_sort
-#################################################
-# input: arr
-# idea: move smallest to front, move second smallest after, move third...
-# time: O(N^2)
-# stable: no
-# in-place: yes
+"""
+algo: selection_sort
+input: arr
+idea: move smallest to front, move 2nd smallest after, move 3rd...
+time: O(N^2)
+stable: no
+in-place: yes
+"""
 
 def selection_sort(arr):
     n = len(arr)
     for i in range(n):
-        min_index = i
+        # set min to i
+        min = i
+        # find min from i to n
         for j in range(i, n):
-            if arr[j] < arr[min_index]:
-                min_index = j
-        arr[i], arr[min_index] = arr[min_index], arr[i]
+            if arr[j] < arr[min]:
+                min = j
+        # swap position of i and min
+        arr[i], arr[min] = arr[min], arr[i]
     return arr
 
-def test_selection_sort():
-  print(selection_sort([1, 9, 2, 6, -1, 4, 4, 8, 2]))
+def test2():
+  assert selection_sort([1, 9, 2, 6, -1, 4]) == [-1, 1, 2, 4, 6, 9]
+  assert selection_sort([4, 1, 5, 7, 1, 2]) == [1, 1, 2, 4, 5, 7]
+  assert selection_sort([9, 8, 7, 6]) == [6, 7, 8, 9]
+  print("Test Cases Passed!")
 
-#################################################
-# bubble_sort
-#################################################
-# input: arr
-# idea: bubble largest elements to the back iteratively
-# time: O(N^2) but O(N) if almost sorted
-# stable: yes
-# in-place: yes
+"""
+algo: selection_sort
+input: arr
+idea: bubble largest elements to the back iteratively
+time: O(N^2) but O(N) if almost sorted
+stable: yes
+in-place: yes
+"""
 
 def bubble_sort(arr):
     n = len(arr)
+    # iterate from start to n, start to n-1, start to n-2...
     for i in reversed(range(n)):
         swapped = False
         for j in range(i):
+            # bubble this ish to the back
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
                 swapped = True
+        # if no swaps in one iteration :)
         if not swapped:
             return arr
     return arr
 
-def test_bubble_sort():
-  print(bubble_sort([1, 9, 2, 6, -1, 4, 4, 8, 2]))
+def test3():
+  assert bubble_sort([1, 9, 2, 6, -1, 4]) == [-1, 1, 2, 4, 6, 9]
+  assert bubble_sort([4, 1, 5, 7, 1, 2]) == [1, 1, 2, 4, 5, 7]
+  assert bubble_sort([9, 8, 7, 6]) == [6, 7, 8, 9]
+  print("Test Cases Passed!")
 
-#################################################
-# merge_sort
-#################################################
-# input: arr
-# idea: sort left, sort right, merge both
-# time: always O(NlogN)
-# stable: yes
-# in-place: no
+"""
+algo: merge_sort
+input: arr
+idea: sort left, sort right, merge both
+time: always O(NlogN)
+stable: yes
+in-place: no
+"""
 
 def merge_sort(arr):
     n = len(arr)
+    # base case: if 0 or 1 elements, return arr
     if n <= 1:
         return arr
     mid = n // 2
-    left_sorted, right_sorted = merge_sort(arr[:mid]), merge_sort(arr[mid:])
-    left_index, right_index = 0, 0
+    # recursively call merge_sort on left & right halves
+    left_sort, right_sort = merge_sort(arr[:mid]), merge_sort(arr[mid:])
+    left, right = 0, 0
     merged = []
-    while left_index < mid or right_index < n - mid:
-        if left_index == mid:
-            merged.append(right_sorted[right_index])
-            right_index += 1
-        elif right_index == n - mid:
-            merged.append(left_sorted[left_index])
-            left_index += 1
-        elif left_sorted[left_index] <= right_sorted[right_index]:
-            merged.append(left_sorted[left_index])
-            left_index += 1
+    while left < mid or right < n - mid:
+        # no more elements to add from left_sort
+        if left == mid:
+            merged.append(right_sort[right])
+            right += 1
+        # no more elements to add from right_sort
+        elif right == n - mid:
+            merged.append(left_sort[left])
+            left += 1
+        # if left_sort has a smaller element than right_sort
+        elif left_sort[left] <= right_sort[right]:
+            merged.append(left_sort[left])
+            left += 1
         else:
-            merged.append(right_sorted[right_index])
-            right_index += 1
+            merged.append(right_sort[right])
+            right += 1
     return merged
 
-def test_merge_sort():
-  print(merge_sort([1, 9, 2, 6, -1, 4, 4, 8, 2]))
+def test4():
+  assert merge_sort([1, 9, 2, 6, -1, 4]) == [-1, 1, 2, 4, 6, 9]
+  assert merge_sort([4, 1, 5, 7, 1, 2]) == [1, 1, 2, 4, 5, 7]
+  assert merge_sort([9, 8, 7, 6]) == [6, 7, 8, 9]
+  print("Test Cases Passed!")
 
-#################################################
-# quick_sort
-#################################################
-# input: arr
-# setup: set pivot as first element
-# idea: iterate through moving smaller elements to updated pivot location
-# time: O(NlogN) but O(N^2) if almost sorted
-# stable: no
-# in-place: yes
+"""
+algo: quick_sort
+input: arr
+setup: set pivot as first element
+idea: iterate through moving smaller elements to updated pivot location
+time: O(NlogN) but O(N^2) if almost sorted
+stable: no
+in-place: yes
+"""
 
 def quick_sort(arr):
     n = len(arr)
+    # base case: if 0 or 1 elements, return arr
     if n <= 1:
         return arr
     pivot = 0
+    # iterate from 1 to n
     for i in range(1, n):
-         if arr[i] <= arr[0]:
-              pivot += 1
-              arr[pivot], arr[i] = arr[i], arr[pivot]
+        # if element smaller than pivot
+        if arr[i] <= arr[0]:
+            pivot += 1 # move pivot marker up
+            arr[pivot], arr[i] = arr[i], arr[pivot] # and swap
+    # at this point, pivot will point to last element smaller than pivot 
     arr[pivot], arr[0] = arr[0], arr[pivot]
+    # recursively call quick_sort on array left & right of pivot
     return quick_sort(arr[:pivot]) + [arr[pivot]] + quick_sort(arr[pivot+1:])
 
-def test_quick_sort():
-  print(quick_sort([1, 9, 2, 6, -1, 4, 4, 8, 2]))
+def test5():
+  assert quick_sort([1, 9, 2, 6, -1, 4]) == [-1, 1, 2, 4, 6, 9]
+  assert quick_sort([4, 1, 5, 7, 1, 2]) == [1, 1, 2, 4, 5, 7]
+  assert quick_sort([9, 8, 7, 6]) == [6, 7, 8, 9]
+  print("Test Cases Passed!")
 
-#################################################
-# built_in
-#################################################
-# time: O(NlogN)
-# stable: yes
-# in-place: yes
+"""
+algo: built_in
+time: O(NlogN)
+stable: yes
+in-place: yes
+"""
 
 nums = [40, 100, 1, 5, 25, 10]
 nums.sort(reverse=True)
